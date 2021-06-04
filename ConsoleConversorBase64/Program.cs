86a815568c402cf79dca82561f5866716d25083d
Wellington de Andrade Silva <Wellington@gomara.tech>
2021-06-04 10:03:28 -0300

    Add project files.

---
+++ ConsoleConversorBase64/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleConversorBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o caminho do arquivo para conversão:");
+            string path = Console.ReadLine();
+            byte[] arquivoByte = System.IO.File.ReadAllBytes(path);
+            string arqbase64 = System.Convert.ToBase64String(arquivoByte);
+            Console.WriteLine(arqbase64);
+            Console.ReadLine();
+
+        }
+    }
+}
