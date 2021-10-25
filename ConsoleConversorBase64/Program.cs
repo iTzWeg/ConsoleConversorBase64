2b500d25e741a4884c509f174d97661765f2284c
Wellington <wellington.silva@gomara.tech>
2021-10-25 09:37:50 -0300

    Adicionando copia para a área de transferencia

--- ConsoleConversorBase64/Program.cs
+++ ConsoleConversorBase64/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 
+
 namespace ConsoleConversorBase64
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("Insira o caminho do arquivo para conversão:");
             string path = Console.ReadLine();
             byte[] arquivoByte = System.IO.File.ReadAllBytes(path);
             string arqbase64 = System.Convert.ToBase64String(arquivoByte);
+
             Console.WriteLine(arqbase64);
+            System.Windows.Forms.Clipboard.SetText(arqbase64);
+
             Console.ReadLine();
 
         }
